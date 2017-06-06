719864fd9d8c57261898eaeeb05e619b177b56d8
Andrew Mikhailov <a.mihaylov@videal.net>
2017-06-06 18:02:49 +0300

    1. A Redis mutex which is expected to work.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var mutex = require('node-mutex')();
+var redisPort = process.env.redis_PORT;
+var mutex = require('node-mutex')({
+    port: redisPort
+});
 module.exports = {
     Context: function (parameters, key) {
         'use strict';
@@ -19,7 +22,8 @@ module.exports = {
             };
             self.available = true;
             self.wrap = {
-                before: function (target, original, before) {
+                // TODO: Attach code identifier to code
+                before: function (target, original, before, codeIdentifier) {
                     function wrapped() {
                         before.apply(target, arguments);
                         return original.apply(target, arguments);
@@ -27,7 +31,8 @@ module.exports = {
 
                     return wrapped;
                 },
-                delay: function (target, original) {
+                // TODO: Attach code identifier to code
+                delay: function (target, original, codeIdentifier) {
                     function wrapped() {
                         var parameters = arguments;
                         setTimeout(function () {
@@ -37,13 +42,25 @@ module.exports = {
 
                     return wrapped;
                 },
-                mutex: function (target, original) {
+                mutex: function (target, original, codeIdentifier) {
                     function wrapped() {
-
-                        // TODO: Generate a key which will be unique for all injected code
-                        mutex.lock('key', function (error, unlock) {
+                        var value = self._value;
+                        if (codeIdentifier != value.codeIdentifier) {
+                            console.log('nodejs-flow-limit.mutex skipping by code identifier');
+                            original.apply(target, arguments);
+                            return;
+                        }
+                        // This may be useful but for now don't plan to enable that
+                        // if (!value.codeIdentifier || !value.requestIdentifier) {
+                        //     console.log('nodejs-flow-limit no data to build mutex name');
+                        //     original.apply(target, arguments);
+                        //     return;
+                        // }
+                        var mutexName = value.codeIdentifier + ':' + value.requestIdentifier;
+                        console.log('nodejs-flow-limit.mutex trying to acquire lock: ' + mutexName);
+                        mutex.lock(mutexName, function (error, unlock) {
                             if (error) {
-                                console.error('Unable to acquire lock: ', error);
+                                console.error('nodejs-flow-limit.mutex unable to acquire lock: ', error);
                                 return;
                             }
 
